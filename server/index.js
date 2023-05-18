dfbe1f78d8253ca55dab1a62a8db5eea408ee998
Aung-Moe-Oo <70925607+Aung-Moe-Oo@users.noreply.github.com>
2023-05-18 14:49:17 +0630

    update url at server

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:3000",
-    origin: process.env.URL,
+    origin: "https://simple-livechat.vercel.app",
     methods: ["GET", "POST"],
   },
 });
