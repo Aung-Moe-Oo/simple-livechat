89f096f74fab584400cb17815555e468aa5377f2
Aung-Moe-Oo <70925607+Aung-Moe-Oo@users.noreply.github.com>
2023-05-18 16:47:04 +0630

    fix css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import css from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={css.footer}>Made By Aung Moe Oo</div>;
+  return <div className={css.footer}>Made By Sai Aung Moe Oo</div>;
 };
 
 export default Footer;
