d12e3079cb18a609f5e3d71c9f1accbe5d124718
Aung-Moe-Oo <70925607+Aung-Moe-Oo@users.noreply.github.com>
2024-02-16 13:17:52 +0630

    Update Chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import css from "./Chat.module.css";
 
 const Chat = ({ socket, username, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const messagesRef = useRef(null);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -11,15 +14,13 @@ const Chat = ({ socket, username, room }) => {
         author: username,
         room: room,
         message: currentMessage,
-        date:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes(),
+        date: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
       };
 
       await socket.emit("send_message", messageData);
       setMessageList((list) => [...list, messageData]);
       setCurrentMessage("");
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
     }
   };
 
@@ -33,24 +34,34 @@ const Chat = ({ socket, username, room }) => {
     socket.on("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
     });
+    socket.on("join_room", (data) => {
+      return toast.success(`${data.username} joined this room id ${data.room}`, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    });
   }, [socket]);
 
   return (
     <div className={css.container}>
+      <ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover={false} theme="light" />
       <div className={css.header}>
         <p>Live Chat</p>
       </div>
-      <div className={css.chatbody}>
+      <div className={css.chatbody} ref={messagesRef}>
         {messageList.map((list, i) => (
           <div key={i} className={css.message}>
             <div>
               <div
                 className={css.content}
                 style={{
-                  flexDirection:
-                    username === list.author ? "row-reverse" : "row",
-                  // justifyContent:
-                  //   username === list.author ? "flex-end" : "flex-start",
+                  flexDirection: username === list.author ? "row-reverse" : "row",
                   marginLeft: username === list.author ? "auto" : "0",
                 }}
               >
@@ -63,13 +74,7 @@ const Chat = ({ socket, username, room }) => {
         ))}
       </div>
       <div className={css.footer}>
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="hey..."
-          onChange={(e) => setCurrentMessage(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
+        <input type="text" value={currentMessage} placeholder="hey..." onChange={(e) => setCurrentMessage(e.target.value)} onKeyDown={handleKeyDown} />
         <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
